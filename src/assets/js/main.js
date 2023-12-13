91ef6a4d05da58d57c1489722551849b18994f3d
skupta12 <tosya2001@gmail.com>
2023-12-13 20:00:59 +0200

    Header Changes

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -258,6 +258,52 @@
     });
   };
 
+  const addEventOnElem = function (elem, type, callback) {
+    if (elem.length > 1) {
+      for (let i = 0; i < elem.length; i++) {
+        elem[i].addEventListener(type, callback);
+      }
+    } else {
+      elem.addEventListener(type, callback);
+    }
+  }
+
+  GOS.menu_toggle = function() {
+    const navbar = document.querySelector("[data-navbar]");
+    const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+    const navLinks = document.querySelectorAll("[data-nav-link]");
+    const overlay = document.querySelector("[data-overlay]");
+    
+    const toggleNavbar = function () {
+      navbar.classList.toggle("active");
+      overlay.classList.toggle("active");
+    }
+    
+    addEventOnElem(navTogglers, "click", toggleNavbar);
+    
+    const closeNavbar = function () {
+      navbar.classList.remove("active");
+      overlay.classList.remove("active");
+    }
+    
+    addEventOnElem(navLinks, "click", closeNavbar);
+    
+    const header = document.querySelector("[data-header]");
+
+const activeElem = function () {
+  if (window.scrollY > 100) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+}
+
+addEventOnElem(window, "scroll", activeElem);
+  }
+
+ 
+
+
   $(document).ready(function () {
     GOS.scroll_it();
     GOS.swiper();
@@ -265,11 +311,11 @@
     GOS.gallery();
     GOS.scroll_to_top();
     GOS.accordion();
-    GOS.header_change();
+    // GOS.header_change();
   });
 
   $(window).on("scroll", function () {
-    GOS.header_change();
+    // GOS.header_change();
   });
 
   // Window on Resize
